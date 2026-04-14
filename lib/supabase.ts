be0f51042a746dd73cbb6c9568460a30b5aa5fe8
Nicola La Gioia <nico.test@gmail.it>
2026-04-14 19:20:36 +0200

    fix loop4

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true, // Forza il salvataggio della sessione nel localStorage/Cookie
+    autoRefreshToken: true, // Refresh automatico del token per non essere buttati fuori
+    detectSessionInUrl: true, // Serve per gestire i login social o i link di conferma
+  },
+});
